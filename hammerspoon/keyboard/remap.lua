33de42764544c846992e4a648f15cc5b4b5b0f12
Conor Boyd <cboyd@brightcove.com>
2019-09-20 22:42:29 +0100

    hammerspoon keybindings for when they fix it

---
+++ hammerspoon/keyboard/remap.lua
@@ -0,0 +1,12 @@
+-- https://github.com/Hammerspoon/hammerspoon/blob/master/extensions/keycodes/init.lua
+-- need to make changes to the code for this to work
+--hs.hotkey.bind({}, '±', function()-- {{{1
+--    hs.eventtap.keyStroke({}, "Escape")
+--end)-- }}}1
+
+-- this works
+--hs.hotkey.bind({}, '§', function()-- {{{1
+--    hs.eventtap.keyStroke({}, "Escape")
+--end)-- }}}1
+
+-- for now we need to use https://pqrs.org/osx/karabiner/
