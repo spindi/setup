86cb85ce27c3abb350ab3c8db9f27808df1abb39
Conor Boyd <cboyd@brightcove.com>
2020-04-17 15:48:15 +0100

    removing the duplicates and localhost

--- block/get_block.sh
+++ block/get_block.sh
@@ -18,3 +18,7 @@ do
     sed -e 's/ #.*//' | \
     xargs -L 1 >> ${OUTPUT_FILE}
 done
+
+# remove the duplicates
+sort block_file.txt | uniq | grep -v localhost > ${OUTPUT_FILE}.tmp
+mv ${OUTPUT_FILE}.tmp ${OUTPUT_FILE}
