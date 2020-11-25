cfd10cbf65cd25cbb2d815ae45ba33a1aa280463
Conor Boyd <conor.boyd@reason.co>
2020-11-25 11:21:30 +0000

    Adding in scrcpy to take video of mobile screen

---
+++ install/scrcpy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# install
+brew cask install android-platform-tools
+brew install scrcpy
+
+# make sure you have adb debugging turned on
+
+# now run with
+# scrcpy --always-on-top -t
