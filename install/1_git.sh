3ea26e20fe164e4a59f991a41468d4036c6ea39a
Conor Boyd <conor.boyd@reason.co>
2021-05-25 10:20:41 +0100

    Reworking gitconfig

--- install/1_git.sh
+++ install/1_git.sh
@@ -1,9 +1,12 @@
 cd ~
 ln -s -f Setup/git/gitconfig .gitconfig
-ln -s -f Setup/git/gitexcludes .gitexcludes
-ln -s -f Setup/git/gitflow_export .gitflow_export
-ln -s -f Setup/gitignore_global .gitignore_global
 cd -
+
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.co checkout
+git config --global alias.dt difftool
+git config --global alias.st status
 
 # set git default email
 git config --global user.email conor.boyd@spindicator.com
