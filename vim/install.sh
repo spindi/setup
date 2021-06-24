e5628aaaa268c9bd3ca949a7dd92531d1a65b971
Conor Boyd <conor.boyd@rvu.co.uk>
2021-06-24 22:29:40 +0100

    Vim, doq and changing path

--- vim/install.sh
+++ vim/install.sh
@@ -1,5 +1,5 @@
 # sudo apt install vim
-brew install vim
+brew install macvim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -s ~/Setup/vim/vimrc ~/.vimrc 
