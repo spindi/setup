3db901acdee5c63949a68dce601b3b2986d8db1b
Conor Boyd <conor.boyd@rvu.co.uk>
2021-06-24 09:45:09 +0100

    Updates for Mac compat

--- vim/install.sh
+++ vim/install.sh
@@ -1,4 +1,5 @@
-sudo apt install vim
+# sudo apt install vim
+brew install vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -s ~/Setup/vim/vimrc ~/.vimrc 
