698eed65a97e245eb35566abd1354b5999ff474b
Conor Boyd <conor.boyd@rvu.co.uk>
2021-06-28 14:29:47 +0100

    Forgot the select all

--- hammerspoon/keyboard/clipboard_mac.lua
+++ hammerspoon/keyboard/clipboard_mac.lua
@@ -1,3 +1,4 @@
+hs.hotkey.bind({"ctrl"}, "a", nil, function() hs.eventtap.keyStroke({"cmd"}, "a") end)
 hs.hotkey.bind({"ctrl"}, "c", nil, function() hs.eventtap.keyStroke({"cmd"}, "c") end)
 hs.hotkey.bind({"ctrl"}, "v", nil, function() hs.eventtap.keyStroke({"cmd"}, "v") end)
 hs.hotkey.bind({"ctrl"}, "x", nil, function() hs.eventtap.keyStroke({"cmd"}, "x") end)
