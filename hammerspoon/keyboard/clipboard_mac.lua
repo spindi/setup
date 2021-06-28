3de27df3092775c84b2982d11e13b6fda17df27d
Conor Boyd <conor.boyd@rvu.co.uk>
2021-06-28 14:25:11 +0100

    Adding in extra clipboard keys for british pc layout

---
+++ hammerspoon/keyboard/clipboard_mac.lua
@@ -0,0 +1,3 @@
+hs.hotkey.bind({"ctrl"}, "c", nil, function() hs.eventtap.keyStroke({"cmd"}, "c") end)
+hs.hotkey.bind({"ctrl"}, "v", nil, function() hs.eventtap.keyStroke({"cmd"}, "v") end)
+hs.hotkey.bind({"ctrl"}, "x", nil, function() hs.eventtap.keyStroke({"cmd"}, "x") end)
