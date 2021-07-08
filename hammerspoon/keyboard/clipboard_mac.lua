086eedd0268ad7162ed1d68f57147148bf26b6a7
Conor Boyd <conor.boyd@rvu.co.uk>
2021-07-08 20:07:44 +0100

    Clipboard additions for the Mac

--- hammerspoon/keyboard/clipboard_mac.lua
+++ hammerspoon/keyboard/clipboard_mac.lua
@@ -1,4 +1,4 @@
-hs.hotkey.bind({"ctrl"}, "a", nil, function() hs.eventtap.keyStroke({"cmd"}, "a") end)
-hs.hotkey.bind({"ctrl"}, "c", nil, function() hs.eventtap.keyStroke({"cmd"}, "c") end)
-hs.hotkey.bind({"ctrl"}, "v", nil, function() hs.eventtap.keyStroke({"cmd"}, "v") end)
-hs.hotkey.bind({"ctrl"}, "x", nil, function() hs.eventtap.keyStroke({"cmd"}, "x") end)
+hs.hotkey.bind({"alt"}, "a", nil, function() hs.eventtap.keyStroke({"cmd"}, "a") end)
+hs.hotkey.bind({"alt"}, "c", nil, function() hs.eventtap.keyStroke({"cmd"}, "c") end)
+hs.hotkey.bind({"alt"}, "v", nil, function() hs.eventtap.keyStroke({"cmd"}, "v") end)
+hs.hotkey.bind({"alt"}, "x", nil, function() hs.eventtap.keyStroke({"cmd"}, "x") end)
