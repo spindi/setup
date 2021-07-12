0280d18e5f2698d1e651bf392b7283e0ab06c498
Conor Boyd <conor.boyd@rvu.co.uk>
2021-07-12 22:31:00 +0100

    BigQuery and K8s helpers

---
+++ bigquery/install.sh
@@ -0,0 +1,4 @@
+#!env bash
+DIR=/usr/local/bin
+ln -f -s ~/Setup/bigquery/bqc ${DIR}/bqc 
+ln -f -s ~/Setup/bigquery/bqv ${DIR}/bqv 
---
+++ kubernetes/install.sh
@@ -0,0 +1,6 @@
+#!env bash
+DIR=/usr/local/bin
+ln -f -s ~/Setup/kubernetes/ks ${DIR}/ks 
+ln -f -s ~/Setup/kubernetes/kgp ${DIR}/kgp 
+ln -f -s ~/Setup/kubernetes/kl ${DIR}/kl
+ln -f -s ~/Setup/kubernetes/klt ${DIR}/klt
