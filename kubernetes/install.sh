06cd34027516533616cc644c95a8ae54a2a46f50
Conor Boyd <conor.boyd@rvu.co.uk>
2021-07-16 09:41:59 +0100

    Kubernetes helpers

--- kubernetes/install.sh
+++ kubernetes/install.sh
@@ -1,6 +1,15 @@
 #!env bash
+brew install kubernetes-cli
+brew install stern 
+
 DIR=/usr/local/bin
-ln -f -s ~/Setup/kubernetes/ks ${DIR}/ks 
-ln -f -s ~/Setup/kubernetes/kgp ${DIR}/kgp 
-ln -f -s ~/Setup/kubernetes/kl ${DIR}/kl
-ln -f -s ~/Setup/kubernetes/klt ${DIR}/klt
+ln -f -s ~/Setup/kubernetes/ks ${DIR}/ks       # shell
+ln -f -s ~/Setup/kubernetes/kge ${DIR}/kgd     # get deployment 
+ln -f -s ~/Setup/kubernetes/kge ${DIR}/kge     # get endpoints 
+ln -f -s ~/Setup/kubernetes/kgi ${DIR}/kgi     # get ingress
+ln -f -s ~/Setup/kubernetes/kgp ${DIR}/kgp     # get pods
+ln -f -s ~/Setup/kubernetes/kgp ${DIR}/kgr     # get replicaset
+ln -f -s ~/Setup/kubernetes/kgs ${DIR}/kgs     # get service
+ln -f -s ~/Setup/kubernetes/kl ${DIR}/kl       # log
+ln -f -s ~/Setup/kubernetes/klt ${DIR}/klt     # log tail
+ln -f -s ~/Setup/kubernetes/klta ${DIR}/klta   # log tail all
