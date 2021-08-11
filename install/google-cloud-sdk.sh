670b6a08ce228155f65c2b8f098823b95e324eb0
Conor Boyd <conor.boyd@rvu.co.uk>
2021-08-11 11:59:47 +0100

    Updated instructions for WSL

--- install/google-cloud-sdk.sh
+++ install/google-cloud-sdk.sh
@@ -1,1 +1,6 @@
-brew cask install google-cloud-sdk
+#!env bash
+# linux
+# curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-352.0.0-linux-x86_64.tar.gz
+
+# mac only
+brew install --cask google-cloud-sdk
