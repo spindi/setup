b14cde3d2b5d10662440aab2ce5a606977a5997d
Conor Boyd <conor.boyd@rvu.co.uk>
2021-08-25 14:00:18 +0100

    Modification to add clip to shift + forward delete

--- hammerspoon/keyboard/clipboard_mac.lua
+++ hammerspoon/keyboard/clipboard_mac.lua
@@ -2,3 +2,5 @@ hs.hotkey.bind({"alt"}, "a", nil, function() hs.eventtap.keyStroke({"cmd"}, "a")
 hs.hotkey.bind({"alt"}, "c", nil, function() hs.eventtap.keyStroke({"cmd"}, "c") end)
 hs.hotkey.bind({"alt"}, "v", nil, function() hs.eventtap.keyStroke({"cmd"}, "v") end)
 hs.hotkey.bind({"alt"}, "x", nil, function() hs.eventtap.keyStroke({"cmd"}, "x") end)
+-- Without the following the text would not end up in the clipboard
+hs.hotkey.bind({"rightshift"}, "forwarddelete", nil, function() hs.eventtap.keyStroke({"cmd"}, "x") end)
