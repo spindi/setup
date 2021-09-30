320ae3979e69d8692a060fcce69d475e3082edd2
Conor Boyd <conor.boyd@napster.com>
2021-09-30 12:52:22 +0100

    Markdown render for CLI. Useful for checking

---
+++ markdown/wsl/install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# If you get /mnt/c/ errors then you need to restart the WSL subsystem "wsl.exe --shutdown"
+cd /usr/local/bin
+sudo ln -s -f ~/Setup/markdown/wsl/mdr_linux_amd64 ./mdr 
