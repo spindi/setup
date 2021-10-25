3e8353937c01e5c7b454d545aafa15db87a83a82
Conor Boyd <conor.boyd@napster.com>
2021-10-25 11:12:14 +0100

    Pyenv went batshit so I cut it out

---
+++ install/browsh.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://github.com/browsh-org/browsh/releases/download/v1.6.4/browsh_1.6.4_linux_amd64.deb
+sudo apt install ./browsh_1.6.4_linux_amd64.deb
+rm ./browsh_1.6.4_linux_amd64.deb
