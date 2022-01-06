751b026c8643cdc0893ea6e33dddba3149b5c4d8
Conor Boyd <conor.boyd@napster.com>
2022-01-06 10:54:39 +0000

    Adding in the CRC checker

--- install/gcp_sdk.sh
+++ install/gcp_sdk.sh
@@ -1,1 +1,5 @@
-brew install --cask google-cloud-sdk
+#!/bin/bash
+# sudo apt install google-cloud-sdk
+sudo apt install gcc python3-dev python3-setuptools
+sudo pip3 uninstall crcmod
+sudo pip3 install --no-cache-dir -U crcmod
