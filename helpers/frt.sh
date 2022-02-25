8bc5d1f57802b688c0b6e992de42b8f1714cf88e
Conor Boyd <conor.boyd@napster.com>
2022-02-25 19:47:29 +0000

    Fastly real time API helper

--- helpers/frt.sh
+++ helpers/frt.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 # $1 = service_id
-# $2 = timestamp
+# $2 = timestamp (h can be used to get last 120 seconds)
 curl -s -H "Fastly-Key:$FASTLY_API_KEY" "https://rt.fastly.com/v1/channel/$1/ts/$2"
