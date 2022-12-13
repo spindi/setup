8cecc8650446c1bac6433be87004cd5149fc54ed
Conor Boyd <conor.boyd@napster.com>
2022-12-13 11:57:43 +0000

    Change to read multiple pages

--- helpers/f_all_services.sh
+++ helpers/f_all_services.sh
@@ -1,2 +1,3 @@
 #!/home/linuxbrew/.linuxbrew/bin/fish
-f /services | jq -r '(.data[] | {id: .id, name: .attributes.name})'
+#f /services | jq -r '(.data[] | {id: .id, name: .attributes.name})' # does not support pagination
+f '/service?per_page=1000' | jq -r '(.[] | {id: .id, name: .name})'
