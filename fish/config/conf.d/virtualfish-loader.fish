0c7d0c34cd6d8231f89e955295c5b8497894f8ca
Conor Boyd <news@spindicator.com>
2024-02-21 18:52:26 +0000

    Nerfed the virtualfish loaded, will remove in future

--- fish/config/conf.d/virtualfish-loader.fish
+++ fish/config/conf.d/virtualfish-loader.fish
@@ -1,4 +1,4 @@
-set -g VIRTUALFISH_VERSION 2.5.5
-set -g VIRTUALFISH_PYTHON_EXEC /home/linuxbrew/.linuxbrew/opt/python@3.11/bin/python3.11
-source /home/conor.boyd/.local/lib/python3.11/site-packages/virtualfish/virtual.fish
-emit virtualfish_did_setup_plugins
+# set -g VIRTUALFISH_VERSION 2.5.5
+# set -g VIRTUALFISH_PYTHON_EXEC /home/linuxbrew/.linuxbrew/opt/python@3.11/bin/python3.11
+# source $HOME/.local/lib/python3.11/site-packages/virtualfish/virtual.fish
+# emit virtualfish_did_setup_plugins
