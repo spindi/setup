a48d37facc3664c9217087986c5d59e09fc57aaa
Conor Boyd <news@spindicator.com>
2024-02-25 12:24:58 +0000

    Screen layout scripts to work with both p14s and x1

---
+++ screenlayout/hdmi_above_centre_x1.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --mode 2560x1440 --pos 640x2160 --rotate normal --output DP-1 --off --output HDMI-1 --mode 3840x2160 --pos 0x0 --rotate normal --output DP-2 --off --output HDMI-2 --off
