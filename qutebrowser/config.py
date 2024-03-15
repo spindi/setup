22b7f063b1c1881c5f8322ccca54395bb69b1124
Conor Boyd <conor.boyd@planet.com>
2024-03-15 19:15:25 +0000

    Espanso and Qutebrowser

---
+++ qutebrowser/config.py
@@ -0,0 +1,4 @@
+# previous user agent
+# Mozilla/5.0 ({os_info}) AppleWebKit/{webkit_version} (KHTML, like Gecko) {qt_key}/{qt_version} {upstream_browser_key}/{upstream_browser_version} Safari/{webkit_version}
+# suggested user agent
+# Mozilla/5.0 (X11; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/77.1
