14fa18e8fa9e91850fdd4a8ee4a77cf634ee621b
Conor Boyd <conor.boyd@planet.com>
2024-03-15 11:31:15 +0000

    Adding git blame

---
+++ nvim/lua/plugins/git_blame.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "f-person/git-blame.nvim",
+    event = "BufRead",
+    config = function()
+      vim.cmd("highlight default link gitblame SpecialComment")
+      vim.g.gitblame_enabled = 0
+      vim.g.gitblame_message_template = "  <summary> • <date> • <author>"
+    end,
+  },
+}
