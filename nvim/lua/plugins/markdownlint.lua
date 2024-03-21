4ad3e0e316d6eaf518c0ae9c61759363bb2b015f
Conor Boyd <conor.boyd@planet.com>
2024-03-21 13:52:46 +0000

    Disable long line nagging from markdownlint

---
+++ nvim/lua/plugins/markdownlint.lua
@@ -0,0 +1,13 @@
+-- disable specific errors when linting
+return {
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters = {
+        markdownlint = {
+          args = { "--disable", "MD013", "--" },
+        },
+      },
+    },
+  },
+}
