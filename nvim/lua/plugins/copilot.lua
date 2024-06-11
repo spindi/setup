3cb142a84a8dd0f8871ad44d567150beb1f4ec16
Conor Boyd <conor.boyd@planet.com>
2024-06-11 16:14:23 +0100

    LazyVim updates, remove telescope

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -4,8 +4,8 @@ return {
     cmd = "Copilot",
     build = ":Copilot auth",
     opts = {
-      suggestion = { enabled = false },
-      panel = { enabled = false },
+      suggestion = { enabled = true },
+      panel = { enabled = true },
       filetypes = {
         markdown = true,
         help = true,
