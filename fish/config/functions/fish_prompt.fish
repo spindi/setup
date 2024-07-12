732c27df74b7b723ef06a4d307c69c9e4ffd4089
Conor Boyd <conor.boyd@planet.com>
2024-07-12 14:34:48 +0100

    Updated git status

--- fish/config/functions/fish_prompt.fish
+++ fish/config/functions/fish_prompt.fish
@@ -18,12 +18,18 @@ function fish_prompt
   #set -l fish     "▶◀▶"
   #set -l fish     "▶"
   set -l fish     \ue00a # 
-  set -l ahead    "↑"
-  set -l behind   "↓"
-  set -l diverged "⥄"
-  set -l dirty    "⨯"
-  set -l stash    "≡"
-  set -l none     "◦"
+  #set -l ahead    "↑"
+  #set -l behind   "↓"
+  #set -l diverged "⥄"
+  #set -l dirty    "⨯"
+  #set -l stash    "≡"
+  #set -l none     "◦"
+  set -l ahead    "ahead"
+  set -l behind   "behind"
+  set -l diverged "diverged"
+  set -l dirty    \uf2fe # 
+  set -l stash    "stash"
+  set -l none     ""
 
   set -l normal_color     (set_color normal)
   set -l success_color    (set_color cyan)
