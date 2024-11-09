e5bf43031e4596fd1017f551ec46a0e3d107e73e
Conor Boyd <news@spindicator.com>
2024-11-09 14:40:45 +0000

    Move to pipx

--- install/qutebrowser.fish
+++ install/qutebrowser.fish
@@ -1,5 +1,5 @@
 #!/home/linuxbrew/.linuxbrew/bin/fish
 
 brew install pyqt@6
-pip install qutebrowser PyQt6 PyQt6-WebEngine
+pipx install qutebrowser PyQt6 PyQt6-WebEngine
 sudo apt install gstreamer1.0-plugins-{bad,base,good,ugly}
