2b397fd96683d33926a56856ffa3ea7993fa6562
Conor Boyd <conor.boyd@planet.com>
2024-12-17 15:02:40 +0000

    Remove copilot_cmp

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,12 +1,14 @@
 return {
   {
     "zbirenbaum/copilot.lua",
-    enabled = true,
     cmd = "Copilot",
     event = "InsertEnter",
+    config = function()
+      require("copilot").setup({})
+    end,
     opts = {
-      suggestion = { enabled = false },
-      panel = { enabled = false },
+      suggestion = { enabled = true, auto_trigger = true },
+      panel = { enabled = true, auto_refresh = true },
     },
   },
 }
--- nvim/lua/plugins/nvim_cmp.lua
+++ nvim/lua/plugins/nvim_cmp.lua
@@ -1,17 +1,15 @@
 return {
   {
-    "nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-emoji",
-      {
-        "zbirenbaum/copilot-cmp",
-        opts = {},
-      },
-    },
+    "hrsh7th/nvim-cmp",
+    optional = true,
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
-      local cmp = require("cmp")
-      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "copilot" }, { name = "emoji" } }))
+      table.insert(opts.sources, { name = "emoji" })
+      table.insert(opts.sources, 1, {
+        name = "copilot",
+        group_index = 1,
+        priority = 100,
+      })
     end,
   },
 }
