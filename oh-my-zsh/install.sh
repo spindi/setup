de16ea53ac36f49ca80ec6a32f53541026e5f497
Conor Boyd <news@spindicator.com>
2025-04-18 20:22:11 +0100

    OMZ

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -1,1 +1,19 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# plugins
+# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# git clone https://github.com/zsh-users/zsh-history-substring-search
+# git clone https://github.com/zdharma-continuum/fast-syntax-highlighting
+
+brew install git
+brew install coreutils # gls amongst others
+brew install font-fira-code-nerd-font # ligatures
+brew install fzf
+brew install vivid # lscolors
+brew install lazygit
+brew install karabiner-elements # replacing the top left stupid key on a brit keyb
+brew install mc
+brew install go
+brew install node # needed for some of the lspconfig stuff in mason
+brew install ripgrep
+brew install fd # export FZF_DEFAULT_COMMAND='fd'
