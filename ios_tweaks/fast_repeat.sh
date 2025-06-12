efe7a956874a4f2f54e75f5b2497666915b294eb
Conor Boyd <conor.boyd@partnerize.com>
2025-06-12 11:35:05 +0100

    ios tweaks

---
+++ ios_tweaks/fast_repeat.sh
@@ -0,0 +1,8 @@
+# os x
+
+# repeat initial delay / normal minimum is 15 (225 ms)
+defaults write -g InitialKeyRepeat -float 10.0
+# repeat delay / normal minimum is 2 (30 ms)
+defaults write -g KeyRepeat -float 1.0
+
+# reboot
