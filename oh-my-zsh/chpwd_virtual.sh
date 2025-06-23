73fc306b27a32160dd89bdd30df9063731dde894
Conor Boyd <conor.boyd@partnerize.com>
2025-06-23 14:54:43 +0100

    Fix deactivate bug

--- oh-my-zsh/chpwd_virtual.sh
+++ oh-my-zsh/chpwd_virtual.sh
@@ -2,22 +2,22 @@ BASE="/Users/conor.boyd/Documents/Repo"
 
 function chpwd() {
   PWD=$(pwd)
-  DIRNAME=$(dirname ${PWD})
-  BASENAME=$(basename ${PWD})
+  DIRNAME=$(dirname $PWD)
+  BASENAME=$(basename $PWD)
 
   # make sure we are in repo
   if [[ $DIRNAME = $BASE ]];
   then
     # dir specific actions
-    if [[ $BASENAME =~ "^(pz-tracking-cache-binlog|pz-data-generator)" ]]; 
+    if [[ $BASENAME =~ "^(pz-tracking-cache-binlog|pz-tracking-data-generator|pz-tracking)$" ]]; 
     then
       source "${BASE}/${BASENAME}/.venv/bin/activate"
-    else
-      # deactivate if we are in a virtual
-      if [[ $VIRTUAL_ENV_PROMPT != "" ]];
-      then
-        deactivate
-      fi
+    fi
+  else
+    # not in repo, if still active, deactivate
+    if [[ $VIRTUAL_ENV_PROMPT != "" ]];
+    then
+      deactivate
     fi
   fi
 }
