9cd648ff814b4eb6d1326cdcd8a3121b79eac413
Conor Boyd <conor.boyd@partnerize.com>
2025-06-23 12:11:04 +0100

    Per repo activations

--- oh-my-zsh/chpwd_virtual.sh
+++ oh-my-zsh/chpwd_virtual.sh
@@ -2,15 +2,22 @@ BASE="/Users/conor.boyd/Documents/Repo"
 
 function chpwd() {
   PWD=$(pwd)
+  DIRNAME=$(dirname ${PWD})
+  BASENAME=$(basename ${PWD})
 
-  if [[ $PWD = "${BASE}/pz-tracking-cache-binlog" ]]; 
+  # make sure we are in repo
+  if [[ $DIRNAME = $BASE ]];
   then
-    source "${BASE}/pz-tracking-cache-binlog/.venv/bin/activate"
-  else
-    # deactivate if we are in a virtual
-    if [[ $VIRTUAL_ENV_PROMPT != "" ]];
+    # dir specific actions
+    if [[ $BASENAME =~ "^(pz-tracking-cache-binlog|pz-data-generator)" ]]; 
     then
-      deactivate
+      source "${BASE}/${BASENAME}/.venv/bin/activate"
+    else
+      # deactivate if we are in a virtual
+      if [[ $VIRTUAL_ENV_PROMPT != "" ]];
+      then
+        deactivate
+      fi
     fi
   fi
 }
