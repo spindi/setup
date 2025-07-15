105e2a7b13beeb96781ae4c1f70cc83462d3c156
Conor Boyd <conor.boyd@partnerize.com>
2025-07-15 08:59:49 +0100

    Vim conifg pydoc anad copilot

--- nvim/lua/plugins/vim_pydocstring.lua
+++ nvim/lua/plugins/vim_pydocstring.lua
@@ -2,7 +2,7 @@ return {
   {
     'heavenshell/vim-pydocstring',
     init = function()
-      vim.g.pydocstring_doq_path = '/home/linuxbrew/.linuxbrew/bin/doq'
+      vim.g.pydocstring_doq_path = '/Users/conor.boyd/.local/bin/doq'
     end,
   },
 }
