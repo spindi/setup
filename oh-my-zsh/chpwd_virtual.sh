a242a9f7502ec5f9d81a787b27a9469f658a071c
Conor Boyd <conor.boyd@partnerize.com>
2025-07-23 13:07:48 +0100

    Fix for subdirs

--- oh-my-zsh/chpwd_virtual.sh
+++ oh-my-zsh/chpwd_virtual.sh
@@ -2,16 +2,15 @@ BASE="/Users/conor.boyd/Documents/Repo"
 
 function chpwd() {
   PWD=$(pwd)
-  DIRNAME=$(dirname $PWD)
-  BASENAME=$(basename $PWD)
+  DIR=$(pwd | sed -e 's/.*Repo\///' | sed -e 's/\/.*//')
 
   # make sure we are in repo
-  if [[ $DIRNAME = $BASE ]];
+  if [[ $PWD == *"$BASE"* && $PWD != $BASE ]];
   then
     # dir specific actions
-    if [[ $BASENAME =~ "^(pz-tracking-cache-binlog|pz-tracking-data-generator|pz-tracking)$" ]]; 
+    if [[ $DIR =~ "^(pz-tracking-cache-binlog|pz-tracking-data-generator|pz-tracking|graphql-strawberry|graphql-strawberry-django|pz-trackman)$" ]]; 
     then
-      source "${BASE}/${BASENAME}/.venv/bin/activate"
+      source "${BASE}/${DIR}/.venv/bin/activate"
     fi
   else
     # not in repo, if still active, deactivate
