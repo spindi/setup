337a2cfc67d8280865a1b34f394abb75c42ff8ab
Conor Boyd <conor.boyd@performancehorizon.com>
2026-01-26 10:37:41 +0000

    Mac ulimits

---
+++ ulimit/mac/limits.sh
@@ -0,0 +1,3 @@
+echo 'kern.maxfiles=20480' | sudo tee -a /etc/sysctl.conf
+echo -e 'limit maxfiles 8192 20480\nlimit maxproc 1000 2000' | sudo tee -a /etc/launchd.conf
+echo 'ulimit -n 4096' | sudo tee -a /etc/profile
