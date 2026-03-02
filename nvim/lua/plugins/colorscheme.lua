337e97efde13225583107b11342394ef9c80eb86
Conor Boyd <conor.boyd@performancehorizon.com>
2026-03-02 10:19:23 +0000

    Override the background colour

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,16 @@
 return {
-  -- {
-  --   "catppuccin/nvim",
-  --   lazy = false,
-  --   name = "catppuccin",
-  --   priority = 1000,
-  -- }
   {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    opts = {},
+    opts = {
+      on_highlights = function(hl, c)
+        -- Override main editor background
+        hl.Normal = { bg = "#101010" } -- Your hex color
+        -- Override float/popup backgrounds
+        -- hl.NormalFloat = { bg = "#1a1b26" }
+        -- hl.FloatBorder = { bg = "#1a1b26" }
+      end,
+    },
   },
 }
